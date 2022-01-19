ee78ad1661a547fe97e8d72529bc06f1c9f1d403
Dmitry Khilko <dmitry.jilko@gmail.com>
2022-01-19 20:09:50 +0100

    test eventEmmiter

---
+++ src/app/intermedio/eventemmiter/jugador2.spec.ts
@@ -0,0 +1,24 @@
+import { Jugador2 } from './jugador2';
+
+describe('Jugador2 emit', ()=>{
+ let jugador:Jugador2;
+ beforeEach(()=>jugador=new Jugador2());
+ it('debe de emitir un evento cuando recibe daño',()=>{
+   let nuevoHp=0;
+   jugador.hpCambia.subscribe(hp=>{
+     nuevoHp=hp;
+
+   })
+   jugador.recibeDanio(1000);
+   expect(nuevoHp).toBe(0);
+ })
+
+ it('debe de emitir un evento cuando recibe daño y sobrevivir si es menos de 100',()=>{
+   let nuevoHp=0;
+   jugador.hpCambia.subscribe(hp=>
+     nuevoHp=hp )
+   jugador.recibeDanio(50);
+   expect(nuevoHp).toBe(50);
+ })
+
+})
